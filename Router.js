4c7f50cdccab29e098b9db23aff8eda1973dd0f3
SigmaSoldi3R <pablobc.1995@gmail.com>
2016-12-17 15:23:07 +0100

    Code refacotring

--- Router.js
+++ Router.js
@@ -1,102 +1,99 @@
- 'use strict'
- /**
- * Simplistic Routing Module
- * NodeJS Regular expression routing.
- * ---
- * Argochamber Interactive 2016
- * by Pablo 'sigmasoldier' Blanco Celdrán
- */
+'use strict'
+/**
+* Simplistic Routing Module
+* NodeJS Regular expression routing.
+* ---
+* Argochamber Interactive 2016
+* by Pablo 'sigmasoldier' Blanco Celdrán
+*/
+
+/**
+* This is an atomic hanler.
+* Those store simple data as the callback.
+*/
+function Handler(regex, callback, passable){
+
+ this.callback = callback;
+
+ this.regex = regex;
+
+ this.isPassable = () => {
+   return passable;
+ };
+
+}
+
+/**
+* The router module grants you a routing system using regexp.
+* Is like using if ... else if ... else with regexp.
+*/
+function Router(){
+
+ let routes = [];
+ let final = () => {};
 
  /**
-  * This is an atomic hanler.
-  * Those store simple data as the callback.
+  * Called when the router wants to handle an incoming connection.
+  * When specify blocking routes.
+  * That means that if the next callback is not a 'if' the regular expression
+  * is not even tested.
   */
- function Handler(regex, callback, passable){
+ this.when = (regex, callback) => {
+   routes.push(new Handler(regex, callback, false));
+   return this;
+ };
 
-   this.callback = callback;
+ /**
+  * Those are tested even if the previous regex was true.
+  */
+ /*this.if = (regex, callback) => {
+   routes.push(new Handler(regex, callback, true));
+   return this;
+ };*/
 
-   this.regex = regex;
+ /**
+  * This is called if noone of the when routes has been matched.
+  */
+ this.final = (handler) => {
+   final = handler;
+ };
 
-   this.isPassable = () => {
-     return passable;
-   };
-
+ /**
+  * This function gets an iterator for the callback sequence.
+  * This iterator.
+  */
+ function* getRoutesIterator(){
+   //let exit = false;
+   //let ai = 0;
+   yield* [...routes];
+   /*while (!exit){
+     let it go xdddddd
+   }*/
  }
 
  /**
-  * The router module grants you a routing system using regexp.
-  * Is like using if ... else if ... else with regexp.
+  * Call this when do you want to apply a route to an incoming URL.
   */
- function Router(){
-
-   let routes = [];
-   let final = () => {};
-
-   /**
-    * Called when the router wants to handle an incoming connection.
-    * When specify blocking routes.
-    * That means that if the next callback is not a 'if' the regular expression
-    * is not even tested.
-    */
-   this.when = (regex, callback) => {
-     routes.push(new Handler(regex, callback, false));
-     return this;
-   };
-
-   /**
-    * Those are tested even if the previous regex was true.
-    */
-   /*this.if = (regex, callback) => {
-     routes.push(new Handler(regex, callback, true));
-     return this;
-   };*/
-
-   /**
-    * This is called if noone of the when routes has been matched.
-    */
-   this.final = (handler) => {
-     final = handler;
-   };
-
-   /**
-    * This function gets an iterator for the callback sequence.
-    * This iterator.
-    */
-   function* getRoutesIterator(){
-     //let exit = false;
-     //let ai = 0;
-     yield* [...routes];
-     /*while (!exit){
-       let it go xdddddd
-     }*/
-   }
-
-   /**
-    * Call this when do you want to apply a route to an incoming URL.
-    */
-   this.listen = (url, response) => {
-     let exit = false;
-     let iter = getRoutesIterator();
-     let last = {done: false};
-     while (!exit && !last.done){
-       last = iter.next();
-       if (!last.done){
-         if (last.value.regex.test(url)){
-           let match = url.match(last.value.regex);
-           last.value.callback(url, response, match);
-           exit = true;
-         }
+ this.listen = (url, response) => {
+   let exit = false;
+   let iter = getRoutesIterator();
+   let last = {done: false};
+   while (!exit && !last.done){
+     last = iter.next();
+     if (!last.done){
+       if (last.value.regex.test(url)){
+         let match = url.match(last.value.regex);
+         last.value.callback(url, response, match);
+         exit = true;
        }
      }
-     if (!exit){
-       final(url, response);
-     }
    }
+   if (!exit){
+     final(url, response);
+   }
+ }
 
- };
+};
 
- //Expose the module.
- module.exports = new Router();
-
-
+//Expose the module.
 module.exports = new Router();
